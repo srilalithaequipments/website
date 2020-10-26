e88d4b0beb1e4b7dbedf82542bdb8bf23853036c
nayan-agrawal <nayanagrawal939@gmail.com>
2020-10-27 00:04:26 +0530

    Creating Components Following components along with the assets folder are added. 1. NavBar 2. Intro 3. AboutUs 4. ActionSection 5. Facts 6. FeaturedServices 7. Portfolio 8. Services 9. Skills

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './Components/NavBar';
+import Intro from './Components/Intro';
+import FeaturedServices from './Components/FeaturedServices';
+import AboutUs from './Components/AboutUs';
+import Services from './Components/Services';
+import ActionSection from './Components/ActionSection';
+import Skills from './Components/Skills';
+import Facts from './Components/Facts';
+import Portfolio from './Components/Portfolio';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Intro />
+      <div id="main">
+        <FeaturedServices />
+        <AboutUs />
+        <Services />
+        <ActionSection />
+        <Skills />
+        <Facts />
+        <Portfolio />
+      </div>
     </div>
   );
 }
