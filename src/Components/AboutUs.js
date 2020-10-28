db10845d31b8897d60bd3bbc8cc42771125b6b0c
srilalithaequipments <srilalithaequipments@gmail.com>
2020-10-28 14:51:37 +0530

    Basic Modifications in about us

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -6,7 +6,9 @@ const AboutUs = () => {
             <div className="container">
                 <header className="section-header">
                     <h3>About Us</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>
+                        Sri Lalitha Equipments is a renowned crane rental company involved in offering cranes on hire in Central India. We are committed to provide reliable crane services at affordable prices. We are a prominent provider of hydraulic cranes, all terrain cranes, hydraulic telescopic cranes, mobile cranes, and a lot more. These cranes are efficient in lifting materials and performing on and off road operations.
+                    </p>
                 </header>
                 <div className="row about-cols">
                     <div className="col-md-4 wow fadeInUp">
@@ -17,7 +19,6 @@ const AboutUs = () => {
                             </div>
                             <h2 className="title"><a href="#">Our Mission</a></h2>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                             </p>
                         </div>
                     </div>
@@ -30,7 +31,6 @@ const AboutUs = () => {
                             </div>
                             <h2 className="title"><a href="#">Our Plan</a></h2>
                             <p>
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                             </p>
                         </div>
                     </div>
@@ -43,7 +43,6 @@ const AboutUs = () => {
                             </div>
                             <h2 className="title"><a href="#">Our Vision</a></h2>
                             <p>
-                                Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
                             </p>
                         </div>
                     </div>
