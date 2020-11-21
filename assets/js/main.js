ac64e2684429561543c9fad6da3e5cb40357ab86
Sri Lalitha Equipments <srilalithaequipments@gmail.com>
2020-11-21 18:08:54 +0530

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,10 +156,12 @@ jQuery(document).ready(function( $ ) {
   // Clients carousel (uses the Owl Carousel library)
   $(".clients-carousel").owlCarousel({
     autoplay: true,
-    dots: true,
     loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
-    }
+    navSpeed: 5000,
+    slideBy: 2,
+    fluidSpeed: 200,
+    smartSpeed: 1200,
+    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 } }
   });
 
   // Testimonials carousel (uses the Owl Carousel library)
